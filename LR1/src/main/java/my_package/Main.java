2712fbafcaa7876b68ff621dcb7407b2a7c4003a
Sun1tAr <denchik2376@yandex.ru>
2024-09-26 18:11:57 +0300

    изменена проверка на одно число

--- LR1/src/main/java/my_package/Main.java
+++ LR1/src/main/java/my_package/Main.java
@@ -5,14 +5,14 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        boolean flag = true;
-        while (flag) {
-            flag = false;
+//        boolean flag = true;
+        while (true) {
+
             Scanner sc = new Scanner(System.in);
             String string = sc.nextLine();
 
             char[] strChars = string.toCharArray();
-
+            String result = "";
             String num1 = "";
             String num2 = "";
             String operator = "";
@@ -28,55 +28,53 @@ public class Main {
                 } else num2 += strChars[i];
             }
 
-            if (iOperator == Integer.MAX_VALUE || num1 == "" || num2 == "") {
-                System.err.println("На вход подано одно число, измените входные данные: " + "\n");
-                flag = true;
+            if (iOperator == Integer.MAX_VALUE || num1.isEmpty() || num2.isEmpty()) {
+                System.err.println("На вход подано одно число, измените входные данные: ");
+                continue;
             } else {
-                String result = "Произошла ошибка - повторите ввод";
-                if (NumberIdentifier.isRomanNumber(num1) && NumberIdentifier.isRomanNumber(num2)){
+                if (NumberIdentifier.isRomanNumber(num1) && NumberIdentifier.isRomanNumber(num2)) {
                     RomanCalc rom1 = new RomanCalc(num1);
-                    switch (operator){
-                        case "+":
-                            result = rom1.addition(num2);
-                            break;
-                        case "/":
-                            result = rom1.division(num2);
-                            break;
-                        case "-":
-                            result = rom1.substraction(num2);
-                            break;
-                        case "*":
-                            result = rom1.multiplication(num2);
-                            break;
-                        default:
-                    }
+                    result = switch (operator) {
+                        case "+" -> rom1.addition(num2);
+                        case "/" -> rom1.division(num2);
+                        case "-" -> rom1.substraction(num2);
+                        case "*" -> rom1.multiplication(num2);
+                        default -> throw new IllegalArgumentException("Ошибка");
+                    };
 
-                }else if (NumberIdentifier.isArabicNumber(num1) && NumberIdentifier.isArabicNumber(num2)){
+                } else if (NumberIdentifier.isArabicNumber(num1) && NumberIdentifier.isArabicNumber(num2)) {
                     ArabicCalc ar1 = new ArabicCalc(num1);
-                    switch (operator){
-                        case "+":
-                            result = ar1.addition(num2);
-                            break;
-                        case "/":
-                            result = ar1.division(num2);
-                            break;
-                        case "-":
-                            result = ar1.substraction(num2);
-                            break;
-                        case "*":
-                            result = ar1.multiplication(num2);
-                            break;
-                        default:
-                    }
-                }else{
+                    result = switch (operator) {
+                        case "+" -> ar1.addition(num2);
+                        case "/" -> ar1.division(num2);
+                        case "-" -> ar1.substraction(num2);
+                        case "*" -> ar1.multiplication(num2);
+                        default -> throw new IllegalArgumentException("Ошибка");
+                    };
+                } else {
                     System.err.println("Неверный формат числа");
+                    continue;
                 }
-                System.out.println("Результат: \n" + result);
             }
-            if(!flag) {
-                System.out.println("Повторить ввод? \n 1 - Да \n 0 - Нет");
-                if (sc.nextInt() == 1) flag = true;
+            System.out.println("Результат: \n" + result);
+            System.out.println("Повторить ввод? \n 1 - Да \n 0 - Нет");
+            String newIn = sc.nextLine();
+            if (isInt(newIn)) {
+                if (Integer.parseInt(newIn) == 0) break;
+            } else {
+                System.err.println("Введённое значение нераспознано - программа остановлена!");
+                break;
             }
+
+        }
+    }
+
+    public static boolean isInt(String input){
+        try {
+            Integer.parseInt(input);
+            return true;
+        } catch (NumberFormatException e){
+            return false;
         }
     }
 }
